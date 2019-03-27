b6752d468b8a93190fd55364f1fc387724a93279
tomich.trabajo@gmail.com <tomich.trabajo@gmail.com>
2019-03-27 21:29:33 +0100

    some init msg

---
+++ src/main/java/com/tom/estudy/vertx/MyFirstVerticle.java
@@ -0,0 +1,98 @@
+package com.tom.estudy.vertx;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.tom.estudy.vertx.model.Whisky;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.StaticHandler;
+
+public class MyFirstVerticle extends AbstractVerticle {
+
+	// Store our product
+	private Map<Integer, Whisky> products = new LinkedHashMap<>();
+
+	@Override
+	public void start(Future<Void> fut) {
+		createSomeData();
+		Router router = Router.router(vertx);
+		router.route("/").handler(routingContext -> {
+			HttpServerResponse response = routingContext.response();
+			response.putHeader("content-type", "text/html").end("<h1>Hello from my first Vert.x 3 application</h1>");
+		});
+
+		// Serve static resources from the /assets directory
+		router.route("/assets/*").handler(StaticHandler.create("assets"));
+		router.get("/api/whiskies").handler(this::getAll);
+		router.route("/api/whiskies*").handler(BodyHandler.create());
+		router.post("/api/whiskies").handler(this::addOne);
+		router.put("/api/whiskies/:id").handler(this::updateOne);
+		router.delete("/api/whiskies/:id").handler(this::deleteOne);
+
+		vertx.createHttpServer().requestHandler(router::accept).listen(
+				// Retrieve the port from the configuration,
+				// default to 8080.
+				config().getInteger("http.port", 8080), result -> {
+					if (result.succeeded()) {
+						fut.complete();
+					} else {
+						fut.fail(result.cause());
+					}
+				});
+	}
+
+	// Create some product
+	private void createSomeData() {
+		Whisky bowmore = new Whisky("Bowmore 15 Years Laimrig", "Scotland, Islay");
+		products.put(bowmore.getId(), bowmore);
+		Whisky talisker = new Whisky("Talisker 57° North", "Scotland, Island");
+		products.put(talisker.getId(), talisker);
+	}
+
+	private void getAll(RoutingContext routingContext) {
+		routingContext.response().putHeader("content-type", "application/json; charset=utf-8")
+				.end(Json.encodePrettily(products.values()));
+	}
+
+	private void addOne(RoutingContext routingContext) {
+		final Whisky whisky = Json.decodeValue(routingContext.getBodyAsString(), Whisky.class);
+		products.put(whisky.getId(), whisky);
+		routingContext.response().setStatusCode(201).putHeader("content-type", "application/json; charset=utf-8")
+				.end(Json.encodePrettily(whisky));
+	}
+	
+	private void updateOne(RoutingContext routingContext) {
+		String id = routingContext.request().getParam("id");
+		if (id == null) {
+			routingContext.response().setStatusCode(400).end();
+		} else {
+			Integer idAsInteger = Integer.valueOf(id);
+			Whisky whisky = products.get(idAsInteger);
+			System.out.println("Original: " + whisky);
+			final Whisky whiskyMod = Json.decodeValue(routingContext.getBodyAsString(), Whisky.class);
+			System.out.println("Modificacion: " + whiskyMod);
+			whisky.setName(whiskyMod.getName());
+			whisky.setOrigin(whiskyMod.getOrigin());
+			products.put(whisky.getId(), whisky);
+		}
+		routingContext.response().setStatusCode(201).end();
+	}
+
+	private void deleteOne(RoutingContext routingContext) {
+		String id = routingContext.request().getParam("id");
+		if (id == null) {
+			routingContext.response().setStatusCode(400).end();
+		} else {
+			Integer idAsInteger = Integer.valueOf(id);
+			products.remove(idAsInteger);
+		}
+		routingContext.response().setStatusCode(204).end();
+	}
+}
